5e717e9a9036d5e3f75e3a7f0ca73e2b9f8e4096
Matthew Miskovsky <matt.miskovsky@gmail.com>
2024-01-19 20:22:37 -0500

    created base html to use template inheritance

--- renoreddit/views.py
+++ renoreddit/views.py
@@ -3,8 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Misko',
+        'title': 'Subway tile install',
+        'content': 'Installing subway tile is annoying. Pick a large tile!',
+        'date': 'January 19, 2024'
+    },
+        {
+        'author': 'Lia-Marie',
+        'title': 'Shiplap: Still in Style',
+        'content': 'Going for a farmhouse look? Give it a try!',
+        'date': 'January 15, 2024'
+    }
+]
+
 def home (request):
-    return render(request, 'renoreddit/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'renoreddit/home.html', context)
 
 def about (request):
-    return render(request, 'renoreddit/about.html')
+    return render(request, 'renoreddit/about.html', {'title': 'About'})
