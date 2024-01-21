8306454d745322a101384972748807f2ea824d91
Matthew Miskovsky <matt.miskovsky@gmail.com>
2024-01-21 11:51:06 -0500

    added crispy forms to style forms

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'renoreddit.apps.RenoredditConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
