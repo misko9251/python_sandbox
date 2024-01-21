b9e434910a612108937319e83366b8ac4d948522
Matthew Miskovsky <matt.miskovsky@gmail.com>
2024-01-21 10:36:46 -0500

    removed dummy data from views

--- renoreddit/views.py
+++ renoreddit/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 # Use SQL lite for developement and postgres for production
 
-posts = [
-    {
-        'author': 'Misko',
-        'title': 'Subway tile install',
-        'content': 'Installing subway tile is annoying. Pick a large tile! You will never regret it. Lorem ipsum blah blah blah. Installing subway tile is annoying. Pick a large tile! You will never regret it. Lorem ipsum blah blah blah.',
-        'date': 'January 19, 2024'
-    },
-        {
-        'author': 'Lia-Marie',
-        'title': 'Shiplap: Still in Style',
-        'content': 'Going for a farmhouse look? Give it a try! But make sure you buy enough, otherwise you end up like me and go to Home Depot three times! Going for a farmhouse look? Give it a try! But make sure you buy enough, otherwise you end up like me and go to Home Depot three times!',
-        'date': 'January 15, 2024'
-    }
-]
-
 def home (request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'renoreddit/home.html', context)
 
