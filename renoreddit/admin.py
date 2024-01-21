d616ea42fb04ab6ba0f0236c9f5392b70e8fe6e0
Matthew Miskovsky <matt.miskovsky@gmail.com>
2024-01-21 10:50:23 -0500

    registered Post model to view in admin browser

--- renoreddit/admin.py
+++ renoreddit/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register your models here so you can view them in the admin page.
+admin.site.register(Post)
